4595490781369d204e30f70f195a5e34bf38636e
Nikita3141 <ndolgavin@mail.ru>
2021-12-10 15:09:59 +0300

    connect to database

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/lib/pq"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -37,19 +39,26 @@ func logoutPage(w http.ResponseWriter, r *http.Request) {
 
 	//http.Redirect(w, r, "/", http.StatusFound)
 }
-type test_struct struct {
-	name string
-	nickname string
-	phone int
+type user_data struct {
+	id int;
+	login string
+	password int
 }
 
+const (
+	host = "localhost"
+	port = 5432
+	user = "postgres"
+	password = "12345"
+	dbname = "user_data"
+)
 func reqistration(w http.ResponseWriter,r*http.Request){
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		panic(err)
 	}
 	log.Println(string(body))
-	var t test_struct
+	var t user_data
 	err = json.Unmarshal(body, &t)
 	if err != nil {
 		panic(err)
@@ -59,12 +68,33 @@ func reqistration(w http.ResponseWriter,r*http.Request){
 
 
 func main() {
+	psqlconn:= fmt.Sprintf("host=%s port=%d user= %s password=%s dbname=%s sslmode=disable",host,port,user,password,dbname)
 
+	db, err := sql.Open("postgres",psqlconn )
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+	insert into user_data (
+	                      id,
+	                      login,
+	                      password
+	                      )
+	values ($1,$2,$3)`,
+	"2",
+	"test_log",
+	"test_pass",)
+	if err!=nil{
+		log.Panic(err)
+	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handler)
 	mux.HandleFunc("/login",loginPage)
 	mux.HandleFunc("/logout",logoutPage)
-	mux.HandleFunc("/register",reqistration)
+	mux.HandleFunc("/register",reqistration) // get json
 
 	server := http.Server{
 		Addr:         ":8080",
